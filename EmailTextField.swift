fc43a1b364fd07119b6ee60edd5a8363168ac6b0
Thanh Bui <thanhbui@MacBook-Pro.local>
2017-02-10 07:52:10 +0700

    first commit

---
+++ EmailTextField.swift
@@ -0,0 +1,21 @@
+//
+//  EmailTextField.swift
+//  TextFieldMaster
+//
+//  Created by Thanh Bui on 2/9/17.
+//  Copyright © 2017 Thanh Bui. All rights reserved.
+//
+
+import UIKit
+
+class EmailTextField: BaseTextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
